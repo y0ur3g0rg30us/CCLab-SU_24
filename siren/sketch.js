b93e9427357c4f764ac367d13417599d6df0e9ca
y0ur3g0rg30us <jcc9902@nyu.edu>
2024-07-23 11:30:16 +0800

    upload the project files

--- siren/sketch.js
+++ siren/sketch.js
@@ -7,15 +7,20 @@ let sideSpeed = 3;
 let r;
 let size;
 let fish = [];
-let fishCount = 10;
+let fishCount = 20;
 let cx;
 let cy;
 let dia;
 let gs1;
+let bubbleCount = 20;
+let bubbles = [];
+let timer = 10;
+let maxTimer = 10;
+let timerStart = 0;
+let gameOver = false;
 
 function setup() {
-    let cnv=createCanvas(800, 500);
-  cnv.parent("p5-canvas-container")
+  createCanvas(800, 500);
 
   creatureX = width / 2;
   creatureY = height / 2;
@@ -27,52 +32,82 @@ function setup() {
     fish.push({
       x: random(width),
       y: random(height),
-      xSpeed: random(0.5, 1), // Slower speed
-      ySpeed: random(0.5, 1), // Slower speed
+      xSpeed: random(0.5, 1),
+      ySpeed: random(0.5, 1),
       bodyColor: color(random(255), random(255), random(255)),
       finColor: color(random(255), random(255), random(255)),
       eyeColor: color(random(255), random(255), random(255)),
     });
   }
+
+  for (let i = 0; i < bubbleCount; i++) {
+    bubbles.push({
+      x: random(width),
+      y: random(height + 10),
+      d: random(10, 35),
+      bSpeed: 3,
+    });
+  }
+  let cnv = createCanvas(800, 500);
+  cnv.parent("p5-canvas-container");
 }
 
 function draw() {
   background(83, 104, 114); // grayish
 
-  drawCave();
-  
-  ///make sure you add the bubbles
+  if (!gameOver) {
+    drawCave();
 
-  
-  drawUnderwaterPlant(width / 2, height - 50);
-  drawUnderwaterPlant(width -100, height - 50);
-  drawUnderwaterPlant(width -200, height - 50);
-  drawUnderwaterPlant(width -300, height - 50);
-  drawUnderwaterPlant(width -500, height - 50);
-  drawUnderwaterPlant(width -600, height - 50);
-  drawUnderwaterPlant(width -700, height - 50);
-  
-  
-  drawCreature(creatureX, creatureY);
+    drawUnderwaterPlant(width / 2, height - 50);
+    drawUnderwaterPlant(width - 100, height - 100);
+    drawUnderwaterPlant(width - 200, height - 25);
+    drawUnderwaterPlant(width - 300, height - 50);
+    drawUnderwaterPlant(width - 500, height - 75);
+    drawUnderwaterPlant(width - 600, height - 50);
+    drawUnderwaterPlant(width - 700, height - 25);
 
-  // Check keyboard input for creature movement
-  if (keyIsDown(UP_ARROW)) {
-    creatureY -= upSpeed;
-  } else if (keyIsDown(DOWN_ARROW)) {
-    creatureY += downSpeed;
+    drawCreature(creatureX, creatureY);
+
+    if (keyIsDown(UP_ARROW)) {
+      creatureY -= upSpeed;
+    } else if (keyIsDown(DOWN_ARROW)) {
+      creatureY += downSpeed;
+    }
+
+    if (keyIsDown(LEFT_ARROW)) {
+      creatureX -= sideSpeed;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      creatureX += sideSpeed;
+    }
+
+    for (let i = fish.length - 1; i >= 0; i--) {
+      moveFish(fish[i]);
+      drawFish(fish[i]);
+
+      if (
+        fish[i].y > creatureY - 60 &&
+        fish[i].y < creatureY - 20 &&
+        fish[i].x > creatureX - 20 &&
+        fish[i].x < creatureX + 20
+      ) {
+        fish.splice(i, 1);
+        resetTimer();
+      }
+    }
+
+    drawBubbles();
+
+    updateTimer();
+    displayTimer();
+    displayFishCount();
+
+    if (timer === 0) {
+      gameOver = true;
+    }
+  } else {
+    displayGameOver();
   }
 
-  if (keyIsDown(LEFT_ARROW)) {
-    creatureX -= sideSpeed;
-  } else if (keyIsDown(RIGHT_ARROW)) {
-    creatureX += sideSpeed;
-  }
-
-  for (let i = 0; i < fish.length; i++) {
-    moveFish(fish[i]);
-    drawFish(fish[i]);
-  }
-  
 }
 
 function drawCave() {
@@ -129,6 +164,18 @@ function drawCreature(x, y) {
 }
 
 function moveFish(fish) {
+  let distance = dist(fish.x, fish.y, creatureX, creatureY);
+  let avoidanceDistance = 200;
+
+  if (distance < avoidanceDistance) {
+    let angle = atan2(fish.y - creatureY, fish.x - creatureX);
+    fish.xSpeed = cos(angle) * 2;
+    fish.ySpeed = sin(angle) * 2;
+  } else {
+    fish.xSpeed = random(0.5, 1);
+    fish.ySpeed = random(0.5, 1);
+  }
+
   fish.x += fish.xSpeed;
   fish.y += fish.ySpeed;
 
@@ -147,23 +194,20 @@ function moveFish(fish) {
 }
 
 function drawFish(fish) {
-  //fin
   fill(fish.finColor);
   arc(fish.x - 30, fish.y, r - 8, r - 10, PI / 2, PI + HALF_PI);
 
-  //body
   fill(fish.bodyColor);
   ellipse(fish.x, fish.y, size * 30, size * 15);
 
-  //eye
   fill(fish.eyeColor);
   ellipse(fish.x + 10, fish.y - 2, 7, 5);
 }
+
 function drawUnderwaterPlant(x, y) {
   push();
   translate(x, y);
 
-  // plant base
   stroke(0, 100, 0);
   strokeWeight(4);
   fill(0, 150, 0);
@@ -176,7 +220,6 @@ function drawUnderwaterPlant(x, y) {
   }
   endShape(CLOSE);
 
-  // branches added
   for (let i = 0; i < 6; i++) {
     let angle = random(PI / 6, PI / 3) * (random() < 0.5 ? -1 : 1);
     let length = random(100, 150);
@@ -216,4 +259,50 @@ function drawLeaf(x, y, size) {
   }
   endShape(CLOSE);
   pop();
+}
+
+function drawBubbles() {
+  for (let i = 0; i < bubbles.length; i++) {
+    let bubble = bubbles[i];
+    push();
+    fill(255, 100);
+    ellipse(bubble.x, bubble.y, bubble.d);
+
+    bubble.y -= bubble.bSpeed;
+    if (bubble.y < -10) {
+      bubble.y = height + 10;
+    }
+    pop();
+  }
+}
+function resetTimer() {
+  timer = maxTimer;
+  timerStart = millis();
+}
+
+function updateTimer() {
+  let elapsedTime = millis() - timerStart;
+  timer = maxTimer - floor(elapsedTime / 1000);
+  timer = constrain(timer, 0, maxTimer);
+}
+
+function displayTimer() {
+  fill(255);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  text("Lose In: " + timer, creatureX, creatureY - 90);
+}
+
+function displayFishCount() {
+  fill(255);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  text("Fish Count: " + fish.length, 10, 10);
+}
+
+function displayGameOver() {
+  fill("red");
+  textSize(48);
+  textAlign(CENTER, CENTER);
+  text("YOU DID GREAT", width / 2, height / 2);
 }
